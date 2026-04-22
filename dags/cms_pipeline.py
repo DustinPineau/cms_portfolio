af709d7a357cd7ff7e5157cfb949039d20b25212
Dustin Pineau <dustinpineau@outlook.com>
2026-04-22 14:28:45 -0700

    add timeouts to all DAG tasks

--- dags/cms_pipeline.py
+++ dags/cms_pipeline.py
@@ -24,6 +24,7 @@ with DAG(
         task_id='download_nppes_weekly',
         ssh_conn_id='ssh_arch_host',
         command='source ~/.bash_profile && cd /mnt/storage2/Projects/cms_ingest && .venv/bin/python nppes_weekly_update.py',
+        cmd_timeout=3600, # 1 hour
     )
 
     # task 2: refresh stg.nppes
@@ -31,6 +32,7 @@ with DAG(
         task_id='refresh_nppes_weekly',
         postgres_conn_id='postgres_portfolio',
         sql='CALL adm.refresh_nppes_weekly();',
+        execution_timeout=timedelta(hours=1),
     )
 
     # task 3: update dim_provider
@@ -38,6 +40,7 @@ with DAG(
         task_id='update_dim_provider',
         postgres_conn_id='postgres_portfolio',
         sql='CALL adm.update_dim_provider();',
+        execution_timeout=timedelta(hours=2),
     )
 
     # task 4: run dbt models
@@ -45,7 +48,9 @@ with DAG(
         task_id='dbt_run',
         ssh_conn_id='ssh_arch_host',
         command='cd /mnt/storage2/Projects/dbt/cms_medicare && .venv/bin/dbt run --select tag:weekly',
+        cmd_timeout=3600, # 1 hour
     )
+
 
     # set dependencies
     download_nppes_weekly >> refresh_nppes_weekly >> update_dim_provider >> dbt_run
